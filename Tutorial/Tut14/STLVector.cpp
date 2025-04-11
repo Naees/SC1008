864896b7ecb820227001aa54b838902e394a9612
Naees <bubblesproximacenturion@gmail.com>
2025-04-12 04:59:31 +0800

    STL vector example

--- Tutorial/Tut14/STLVector.cpp
+++ Tutorial/Tut14/STLVector.cpp
@@ -19,28 +19,41 @@ int main() {
 
     // TO-DO: Add seven daily sales values to the vector: 
     //         120, 200, 150, 80, 90, 220, 100
-    //
-    //
+    std::cout << "Daily Sales: ";
+    dailySales.push_back(120);
+    dailySales.push_back(200);
+    dailySales.push_back(150);
+    dailySales.push_back(80);
+    dailySales.push_back(90);
+    dailySales.push_back(220);
+    dailySales.push_back(100);
 
     // TO-DO: Print all sales values by using an iterator
-    //
-    //
+    for(std::vector<int>::iterator it = dailySales.begin(); it != dailySales.end(); it++) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
 
 
     // TO-DO: Calculate the average of the sales values and print it
-    //
-    //
-
+    // Sum up the values using std::accumulate.
+    int sum = std::accumulate(dailySales.begin(), dailySales.end(), 0);
+    // Compute the average: cast the sum to double to keep the fractional part.
+    double average = static_cast<double>(sum) / dailySales.size();
+    // Print the average with the three digits after the decimal using std::fixed and std::setprecision.
+    std::cout << "Average Sales: " << average << std::endl;
 
 
     // TO-DO: Sort the vector in ascending order using std::sort.
-    //
+    sort(dailySales.begin(), dailySales.end());
 
 
     // TO-DO: Print all the sorted sales values by using an iterator
-    //
-    //
-
+    std::cout << "Sorted Sales: ";
+    for (std::vector<int>::iterator it = dailySales.begin(); it != dailySales.end(); it++){
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
